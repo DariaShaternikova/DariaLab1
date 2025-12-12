1694e90aa835985d657ff31db109f8664a2f401b
Daria Shaternikova <dasha@shaternikova.ru>
2025-12-12 16:48:30 +0300

    Инициализация репозитория: README и базовый скрипт

---
+++ bug_demo.py
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+python3 -c "
+try:
+    from bug_demo import process_data
+    result = process_data([1, 2, 3])
+    exit(0)  # если не упало - хороший коммит
+except IndexError:
+    exit(1)  # если IndexError - плохой коммит
+except:
+    exit(125)  # другие ошибки - пропустить
+"
---
+++ script.ps1
@@ -0,0 +1,4 @@
+﻿function Get-Greeting {
+    param([string] = "Дарья")
+    Write-Output "Привет, ! Добро пожаловать в проект."
+}
