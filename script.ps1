49883e2678575b5115d8abbcfc825202a69d214c
Daria Shaternikova <dasha@shaternikova.ru>
2025-12-19 01:33:21 +0300

    Добавлена функция Get-ProjectInfo и информация о контрибьюторе

--- script.ps1
+++ script.ps1
@@ -2,3 +2,9 @@
     param([string] = "Дарья Шатерникова 2")
     Write-Output "Привет, ! Добро пожаловать в проект."
 }
+
+function Get-ProjectInfo {
+    Write-Output "Проект: Основной проект"
+    Write-Output "Версия: 1.0"
+    Write-Output "Контрибьютор: [Дарья Шатерникова 2]"
+}
