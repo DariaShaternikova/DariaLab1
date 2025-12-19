5f3959cafc57a5a477ff6e8a7ed541c1a4d42755
Daria Shaternikova <dasha@shaternikova.ru>
2025-12-19 14:38:04 +0300

    Initial commit: создание базовой структуры проекта

---
+++ system-info.ps1
@@ -0,0 +1,9 @@
+﻿# Приветствие
+Write-Host "Лабораторная работа по СУРПО" -ForegroundColor Green
+Write-Host "Работа с удаленными репозиториями Git" -ForegroundColor Yellow
+
+# Текущая дата и время
+Get-Date | Format-List
+
+# Информация о системе
+Get-ComputerInfo | Select-Object WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer
